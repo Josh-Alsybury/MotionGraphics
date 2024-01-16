e04073c4e07514d55fcb75751fff1cefb2e4c26f
(Student  C00285802) Josh Alsybury <jalsybury@gmail.com>
2024-01-16 12:51:05 +0000

    work

---
+++ Lab1(motion graphics)/Lab1(motion graphics)/Game.h
@@ -0,0 +1,48 @@
+/// <summary>
+/// author Pete Lowe May 2019
+/// you need to change the above line or lose marks
+/// </summary>
+#ifndef GAME_HPP
+#define GAME_HPP
+/// <summary>
+/// include guards used so we don't process this file twice
+/// same as #pragma once
+/// Don't forget the endif at the bottom
+/// </summary>
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	/// <summary>
+	/// main method for game
+	/// </summary>
+	void run();
+
+private:
+
+	void processEvents();
+	void processKeys(sf::Event t_event);
+	void update(sf::Time t_deltaTime);
+	void render();
+
+	void Walls();
+	void Player();
+
+	sf::RenderWindow m_window; // main SFML window
+	sf::RectangleShape m_wall;
+	sf::RectangleShape m_wall2;
+
+	sf::RectangleShape m_stripe;
+	sf::RectangleShape m_stripe2;
+
+	sf::CircleShape m_player;
+
+	bool m_exitGame; // control exiting game
+
+};
+
+#endif // !GAME_HPP
+
