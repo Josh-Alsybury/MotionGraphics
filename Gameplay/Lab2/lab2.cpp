fdd5214e927e828e4e9a934f506f6181b068b49e
josh <jalsybury@gmail.com>
2024-09-23 18:52:03 +0100

    lab2

--- Gameplay/Lab2/lab2.cpp
+++ Gameplay/Lab2/lab2.cpp
@@ -4,16 +4,17 @@ int main()
 {
 	//example of calling the functions
 
-	int array[4][6] = { {0,0,3,1,3,4},
-					   {0,0,2,3,4,3},
-					   {0,0,1,3,3,2},
-					   {0,0,1,1,1,1} };
+	int array[4][6] = {
+		{0,0,3,1,3,4},
+		{0,0,2,3,4,3},
+		{0,0,1,3,3,2},
+		{0,0,1,1,1,1} 
+	};
+
+
 	PrintArray(array);
-	int score = ReplaceWithMinusOne(array);
-
+	int result = ReplaceWithMinusOne(array);
+	PrintArray(array);
 	FallDownAndReplace(array);
-		
-	
-
 
 }
--- Gameplay/Lab2/lab2.h
+++ Gameplay/Lab2/lab2.h
@@ -2,13 +2,40 @@
 // dont use cout in any of the functions apart from PrintArray();
 int ReplaceWithMinusOne(int array[4][6])
 {
-	return 0;
-}
-void FallDownAndReplace(int array[4][6])
-{
+    bool hasCluster = false;
 
+    // Check for horizontal clusters
+    for (int row = 0; row < 4; row++)
+    {
+        for (int Col = 0; Col < 4; Col++) 
+        {
+            if (array[row][Col] == array[row][Col + 1] && array[row][Col + 1] == array[row][Col + 2])
+            {
+                array[row][Col] = -1;
+                array[row][Col + 1] = -1;
+                array[row][Col + 2] = -1;
+                hasCluster = true;
+            }
+        }
+    }
+
+    // Return whether any cluster was found
+    return hasCluster ? 1 : 0;
 }
+
 void PrintArray(int array[4][6])
+{
+    for (int row = 0; row < 4; row++)
+    {
+        for (int Col = 0; Col < 6; Col++)
+        {
+            std::cout << array[row][Col] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void FallDownAndReplace(int array[4][6])
 {
 
 }
